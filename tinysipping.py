d181e6662e30dddd0910d53a1b7395261ea639c1
Denis Getman <denis.getman@nordigy.ru>
2020-11-12 02:25:51 +0300

    - version bump - added "Request size" feature - reformat code - appearance was quite adjusted

--- tinysipping.py
+++ tinysipping.py
@@ -16,68 +16,91 @@ import argparse
 import time
 import re
 
-VERSION = "0.0.4"
+VERSION = "0.0.5"
 TOOL_DESCRIPTION = "tinysipping is small tool that sends SIP OPTIONS " \
                    "requests to remote host and reads responses. "
 
 MAX_FORWARDS = 70  # times
-PING_TIMEOUT = 10.0  # seconds
+DFL_PING_TIMEOUT = 1.0  # seconds
 MAX_RECVBUF_SIZE = 1400  # bytes
 DFL_SIP_PORT = 5060
 DFL_REQS_COUNT = 0
 DFL_SIP_TRANSPORT = "udp"
 RTT_INFINITE = 99999999.0
 DFL_SEND_PAUSE = 0.5
+DFL_PAYLOAD_SIZE = 600   # bytes
+
+# totally 65536 bytes - max theoretical size of UDP dgram
+PADDING_PATTERN = "the_quick_brown_fox_jumps_over_the_lazy_dog_" * 1489
 
 # messages templates for further formatting
-MSG_SENDING_REQS = "Sending %s SIP OPTIONS request%s from %s:%d to " \
-                   "%s:%d with timeout %.03fs..."
-MSG_RESP_FROM = "SEQ #%d %s: Response from %s (%d bytes, %f sec RTT): %s"
+MSG_SENDING_REQS = "Sending {} SIP OPTIONS request{} (size {}) {}to " \
+                   "{}:{} with timeout {:.03f}s..."
+MSG_RESP_FROM = "SEQ #{} ({} bytes sent) {}: Response from {} ({} bytes, " \
+                "{:.03f} sec RTT): {}"
+
+
+def gen_padding(size=DFL_PAYLOAD_SIZE):
+    """
+    Returns padding for SIP header or body
+    :param size: (int) size of padding
+    :return: (str) prepared padding
+    """
+    return PADDING_PATTERN[:size]
 
 
 def create_sip_req(dst_host, dst_port=DFL_SIP_PORT, src_port=0,
-                   proto=DFL_SIP_TRANSPORT):
+                   proto=DFL_SIP_TRANSPORT, request_size=DFL_PAYLOAD_SIZE):
     """
     Generates serialized SIP header from source data
     :param src_port: (int) source port. Used in From: header
     :param dst_port: (int) destination port. Used in URI and To: header
     :param dst_host: (str) ip address or hostname of remote side.
     :param proto: (str) tcp or udp, otherwise ValueError will raise
+    :param request_size: (int) size that request should be padded to.
     :returns: (string): SIP header in human-readable format. Don"t forget to
                         encode it to bytes
     """
     my_hostname = socket.gethostname()
-    call_id = "{0}-{1}".format(
-        my_hostname,
-        str(uuid.uuid4())
-    )
+    call_id = "{0}-{1}".format(my_hostname, str(uuid.uuid4()))
+
     # According to RFC3261, the branch ID MUST always begin with the characters
     # "z9hG4bK". It used as magic cookie. Beyond this requirement, the precise
     # format of the branch token is implementation-defined
-    branch_id = "z9hG4bK%s" % str(uuid.uuid4())
-    cseq = random.randint(32767, 2147483647)
-    tag_id = random.randint(32767, 2147483647)
-    return "\r\n".join([
-        "OPTIONS sip:options@%s:%d SIP/2.0" % (dst_host, dst_port),
-        "Via: SIP/2.0/%s %s;branch=%s;rport" % (
-            proto.upper(),
-            my_hostname,
-            branch_id
+    branch_id = "z9hG4bK{}".format(str(uuid.uuid4()))
+
+    # these intervals are chosen to keep request size always constant
+    cseq = random.randint(1000000000, 2147483647)
+    tag_id = random.randint(1000000000, 2147483647)
+
+    # HeaDeR WithOut
+    hdr_wo_padding = "\r\n".join([
+        "OPTIONS sip:options@{}:{} SIP/2.0".format(dst_host, dst_port),
+        "Via: SIP/2.0/{} {};branch={};rport".format(
+            proto.upper(), my_hostname, branch_id
         ),
-        "Max-Forwards: %s" % MAX_FORWARDS,
-        "To: <sip:options@%s:%d>" % (dst_host, dst_port),
-        "From: <sip:options@%s:%d>;tag=%d" % (my_hostname, src_port, tag_id),
-        "Call-ID: %s" % call_id,
-        "CSeq: %d OPTIONS" % cseq,
-        "Contact: <sip:options@%s>" % my_hostname,
-        "Accept: application/sdp"
+        "Max-Forwards: {}".format(MAX_FORWARDS),
+        "To: <sip:options@{}:{}>".format(dst_host, dst_port),
+        "From: <sip:options@{}:{}>;tag={}".format(
+            my_hostname, src_port, tag_id
+        ),
+        "Call-ID: {}".format(call_id),
+        "CSeq: {} OPTIONS".format(cseq),
+        "Contact: <sip:tinysipping@{}>".format(my_hostname),
+        "Accept: application/sdp",
         "Content-Length: 0",
+        "P-tinysipping-padding: {}",   # this field will be filled later
         "\r\n"  # for getting double \r\n at the end, as it need by RFC
     ])
 
+    padding_size = request_size - len(hdr_wo_padding) + 2
+    padding = gen_padding(padding_size) if padding_size > 0 else ""
+    request = hdr_wo_padding.format(padding)
+    return request
+
 
 def create_socket(proto=DFL_SIP_TRANSPORT, bind_addr="", bind_port=0,
-                  timeout=PING_TIMEOUT):
+                  timeout=DFL_PING_TIMEOUT):
     """
     Function returns preconfigured socket for transport needs
     :param bind_addr: (str) source host or ip of interface (default "")
@@ -113,6 +136,7 @@ def udp_send(request, params):
         bind_port=params["src_port"],
         timeout=params["timeout"]
     )
+
     try:
         sock.sendto(request.encode(), (dst_ipaddr, params["dst_port"]))
         while True:
@@ -142,6 +166,7 @@ def tcp_send(request, params):
         bind_port=params["src_port"],
         timeout=params["timeout"]
     )
+
     try:
         sock.connect((params["dst_host"], params["dst_port"]))
         sock.sendall(request.encode())
@@ -167,9 +192,9 @@ def _get_params_from_cliargs(args):
         "timeout": (float) Socket timeout
         "proto": Protocol (tcp or udp). Assertion for proto in (tcp, udp)
         "verbose_mode": (bool) Verbose mode
-        ""bad_resp_is_fail"": (bool) Treat 4xx, 5xx, 6xx responses as fail
+        ""ad_resp_is_fail": (bool) Treat 4xx, 5xx, 6xx responses as fail
     }
-    :param args: (argparse.Namespace) argparse CLI arguments
+    :param raw_cli_args: (argparse.Namespace) argparse CLI arguments
     :return: (dict) dictionary with params
     """
     params = {
@@ -183,18 +208,22 @@ def _get_params_from_cliargs(args):
         "verbose_mode": args.verbose_mode,
         "bad_resp_is_fail": args.bad_resp_is_fail,
         "pause_between_transmits": args.pause_between_transmits,
+        "payload_size": args.payload_size,
     }
+
     if ":" in args.destination:
         params["dst_host"], dst_port = args.destination.split(":")
         params["dst_port"] = int(dst_port)
     else:
         params["dst_host"] = args.destination
+
     if args.src_sock:
         if ":" in args.src_sock:
             params["src_host"], src_port = args.src_sock.split(":")
             params["src_port"] = int(src_port)
         else:
             params["src_host"] = args.src_sock
+
     assert (params["proto"] in ("tcp", "udp"))  # tcp and udp support only
     assert (params["dst_host"])  # dst_host is mandatory parameter
     return params
@@ -210,12 +239,15 @@ def _prepare_argv_parser():
         description=TOOL_DESCRIPTION,
         formatter_class=lambda prog: argparse.HelpFormatter(prog, width=120)
     )
+
     ap.add_argument(
         "destination",
-        help="Destination host <dst>[:port] (default port %d)" % DFL_SIP_PORT,
+        help="Destination host <dst>[:port] (default port {})".format(
+            DFL_SIP_PORT),
         type=str,
         action="store",
     )
+
     ap.add_argument(
         "-c",
         dest="count",
@@ -223,6 +255,7 @@ def _prepare_argv_parser():
         type=int,
         default=DFL_REQS_COUNT
     )
+
     ap.add_argument(
         "-p",
         dest="proto",
@@ -231,43 +264,58 @@ def _prepare_argv_parser():
         choices=("tcp", "udp"),
         default=DFL_SIP_TRANSPORT
     )
+
     ap.add_argument(
         "-t",
         dest="sock_timeout",
-        help="Socket timeout in seconds (float, default 10.0)",
+        help="Socket timeout in seconds (float, default {:.01f})".format(
+            DFL_PING_TIMEOUT),
         type=float,
         action="store",
-        default=PING_TIMEOUT
+        default=DFL_PING_TIMEOUT
     )
+
     ap.add_argument(
         "-f",
         dest="bad_resp_is_fail",
         help="Treat 4xx, 5xx, 6xx responses as failed request",
         action="store_true"
     )
+
     ap.add_argument(
         "-l",
         dest="pause_between_transmits",
-        help="Pause between transmits (default 0.5. 0 for immediate send)",
+        help="Pause between transmits (default 0.5, 0 for immediate send)",
         action="store",
         type=float,
         default=DFL_SEND_PAUSE
     )
+
     ap.add_argument(
-        "-s",
+        "-i",
         dest="src_sock",
         help="Source iface [ip/hostname]:[port] (hostname part is optional, "
              "possible to type \":PORT\" form to just set srcport)",
         type=str,
         action="store"
     )
+
     ap.add_argument(
         "-v",
         dest="verbose_mode",
         help="Verbose mode (show sent and received content)",
         action="store_true"
     )
-    ap.add_argument("-V", action="version", version=VERSION)
+
+    ap.add_argument(
+        "-s",
+        dest="payload_size",
+        help="Fill request upto certain size",
+        type=int,
+        action="store",
+        default=DFL_PAYLOAD_SIZE
+    )
+
     return ap
 
 
@@ -302,10 +350,12 @@ def send_one_request(request, params):
         "resp_code": 0,   # response code
         "full_response": "",
     }
+
     ping_func = tcp_send if params["proto"] == "tcp" else udp_send
     start_time = time.time()
     full_response, length, error = ping_func(request=request, params=params)
     end_time = time.time()
+
     if error:
         result["is_successful"] = False
         result["error"] = error
@@ -336,19 +386,23 @@ def calculate_stats(results):
     response_codes = {}
     socket_error_causes = {}
     total_requests = len(results)
+
     for i in results:
         if i["is_successful"]:
             successful_requests += 1
         else:
             failed_requests += 1
+
         if i["rtt"] >= 0:
             min_rtt = i["rtt"] if (i["rtt"] < min_rtt) else min_rtt
             max_rtt = i["rtt"] if (i["rtt"] > max_rtt) else max_rtt
             total_rtt_sum += i["rtt"]
+
         try:
             response_codes[int(i["resp_code"])] += 1
         except KeyError:    # it means there"s no such response code before
             response_codes[int(i["resp_code"])] = 1
+
         if i["error"]:
             cause_name = re.sub(r"\s+", "_", str(i["error"])).lower()
             try:
@@ -357,12 +411,15 @@ def calculate_stats(results):
                 socket_error_causes[cause_name] = 1
         else:
             answered_requests += 1
+
     try:
         del response_codes[0]   # 0 is a stub response code
     except KeyError:
         pass
+
     avg_rtt = -1.0 if not answered_requests \
         else float(total_rtt_sum) / float(answered_requests)
+
     return {
         "total": total_requests,
         "successful": successful_requests,
@@ -381,52 +438,48 @@ def pretty_print_stats(stats):
     Just prints statistics in pretty form
     :param stats: (dict) statistics
     """
+    perc_fmt_str = "{:15s} {:5d}/{:0.3f}%"
+    float_value_str = "{:15s} {:9.3f}"
+
     total_requests = stats["total"]
-    answered_percentage = 0.0 if not stats["answered"] \
+
+    answered_perc = 0.0 if not stats["answered"] \
         else (float(stats["answered"]) / float(total_requests))*100.0
-    success_percentage = 0.0 if not stats["successful"] \
+
+    passed_perc = 0.0 if not stats["successful"] \
         else (float(stats["successful"]) / float(total_requests))*100.0
-    failed_percentage = 0.0 if not stats["failed"] \
+
+    failed_perc = 0.0 if not stats["failed"] \
         else (float(stats["failed"]) / float(total_requests))*100.0
+
     print("\n")
     print("------ FINISH -------")
-    print("Total requests:          %5d" % total_requests)
-    print(
-        "Answered requests:       %5d / %0.3f %%" % (
-            stats["answered"],
-            answered_percentage
-        )
-    )
-    print(
-        "Successful requests:     %5d / %0.3f %%" % (
-            stats["successful"],
-            success_percentage
-        )
-    )
-    print(
-        "Failed requests:         %5d / %0.3f %%" % (
-            stats["failed"],
-            failed_percentage
-        )
-    )
+    print("{:15s} {:5d}".format("Total requests:", total_requests))
+    print(perc_fmt_str.format("Answered:", stats["answered"], answered_perc))
+    print(perc_fmt_str.format("Successful:", stats["successful"], passed_perc))
+    print(perc_fmt_str.format("Failed:", stats["failed"], failed_perc))
+
     print("\n")
+
     if stats["answered"]:
-        print("RTT stats:")
-        print("min.RTT: %0.3f" % stats["min_rtt"])
-        print("avg.RTT: %0.3f" % stats["avg_rtt"])
-        print("max.RTT: %0.3f" % stats["max_rtt"])
+        print("RTT stats (in ms):")
+        print(float_value_str.format("min.RTT:", stats["min_rtt"]))
+        print(float_value_str.format("avg.RTT:", stats["avg_rtt"]))
+        print(float_value_str.format("max.RTT:", stats["max_rtt"]))
         print("\n")
+
     if stats["socket_error_causes"]:
         print("Socket errors causes stats:")
         for k, v in stats["socket_error_causes"].items():
             cause_percentage = 100.0 * (float(v) / float(total_requests))
-            print("%3s: %6s / %0.3f %%" % (k, v, cause_percentage))
+            print("{:15s} {:5s}/{:0.3f}%".format((k, v, cause_percentage)))
         print("\n")
+
     if stats["response_codes"]:
         print("Response codes stats:")
         for k, v in stats["response_codes"].items():
             resp_code_percentage = 100.0 * (float(v) / float(total_requests))
-            print("%3d: %6d / %0.3f" % (k, v,  resp_code_percentage))
+            print(perc_fmt_str.format(str(k), v,  resp_code_percentage))
 
 
 def send_sequential_req_with_print(seq_num, params):
@@ -441,23 +494,26 @@ def send_sequential_req_with_print(seq_num, params):
         dst_port=params["dst_port"],
         src_port=params["src_port"],
         proto=params["proto"],
+        request_size=params["payload_size"]
     )
     result = send_one_request(request, params)
-    print(MSG_RESP_FROM % (
+    _msg_resp = MSG_RESP_FROM.format(
         seq_num,
+        len(request),
         "PASS" if result["is_successful"] else "FAIL",
         params["dst_host"],
         result["length"],
         result["rtt"],
         result["brief_response"],
-        )
     )
+    print(_msg_resp)
     _debug_print(params["verbose_mode"], "Full request:", request)
     _debug_print(
         params["verbose_mode"],
         "Full response:",
         result["full_response"]
     )
+    _debug_print(params["verbose_mode"],"{}\n".format("-" * len(_msg_resp)))
     return result
 
 
@@ -467,16 +523,23 @@ def main():
     """
     params = _get_params_from_cliargs(_prepare_argv_parser().parse_args())
     results = []
-    print(MSG_SENDING_REQS % (
+
+    # Sent from <host:port> substring. Places into resulting message if
+    # <src_interface>:<port> was specified
+    _from_substr = "" if not params["src_host"] and not params["src_port"] \
+        else "from {}:{} ".format(params["src_host"], params["src_port"])
+    sending_req_msg = MSG_SENDING_REQS.format(
         "infinitely" if not params["count"] else params["count"],
         "" if params["count"] == 1 else "s",
-        params["src_host"],
-        params["src_port"],
+        params["payload_size"],
+        _from_substr,
         params["dst_host"],
         params["dst_port"],
         params["timeout"]
-        )
     )
+    print(sending_req_msg)
+    print("{}\n".format("-" * len(sending_req_msg)))
+
     try:
         if not params["count"]:   # 0 means infinite ping
             seq = 0
@@ -493,8 +556,10 @@ def main():
                 if params["pause_between_transmits"]:
                     time.sleep(params["pause_between_transmits"])
     except KeyboardInterrupt:
-        print("\nInterrupted after %d request%s" %
-              (seq, "" if seq == 1 else "s"))
+        print("\nInterrupted after {} request{}".format(
+            seq, "" if seq == 1 else "s")
+        )
+
     pretty_print_stats(calculate_stats(results))
     exit(0)
 
